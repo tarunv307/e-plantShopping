2de80acc78c5ad39a4c21da8c3aa65b17021d23a
TARUN V <tv4002328@gmail.com>
2026-05-26 21:12:54 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+
+      <p>
+        Welcome to Paradise Nursery, your one-stop destination
+        for indoor and outdoor plants.
+      </p>
+
+      <p>
+        We provide healthy plants at affordable prices and help
+        people bring nature closer to their homes.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
