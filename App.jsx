78e0e972eca6d96d7349a4747b0fe69c430a4195
TARUN V <tv4002328@gmail.com>
2026-05-26 21:13:46 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="landing-page">
+      <div className="overlay">
+        <h1>Paradise Nursery</h1>
+
+        <p>Bring Nature Closer to Your Home</p>
+
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
