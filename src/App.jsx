63a0409bed5be26419193d1624041e6a3aa72dc3
TARUN V <tv4002328@gmail.com>
2026-05-26 21:42:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import ProductList from "./ProductList";
 
 function App() {
+
+  const [showProductList, setShowProductList] = useState(false);
+
   return (
-    <div className="landing-page">
-      <div className="overlay">
-        <h1>Paradise Nursery</h1>
 
-        <p>Bring Nature Closer to Your Home</p>
+    <div>
 
-        <button>Get Started</button>
-      </div>
+      {!showProductList ? (
+
+        <div className="landing-page">
+
+          <div className="overlay">
+
+            <h1>Paradise Nursery</h1>
+
+            <p>Bring Nature Closer to Your Home</p>
+
+            <button
+              onClick={() => setShowProductList(true)}
+            >
+              Get Started
+            </button>
+
+          </div>
+
+        </div>
+
+      ) : (
+
+        <ProductList />
+
+      )}
+
     </div>
+
   );
 }
 
